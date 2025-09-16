c725a3c766f72feffc3b6de6593603e2445c0eb7
IsnanAndani <142637953+IsnanAndani@users.noreply.github.com>
2025-09-16 18:21:31 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'mynotes-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  // Add other assets you want to cache
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
